162dffd06958cbb5bd4027b6e7d4778a8227cd55
Krzysztof Jarzyna <ktz@spoko.pl>
2026-06-07 02:23:07 +0100

    Add runtime font example

--- font/font.c
+++ font/font.c
@@ -1,181 +1,246 @@
 /*************************************************************************************
- * GodLib Example Progam Suite
+ * GodLib Example Program Suite
  *
- * Demonstrates 8x8 font printing, including coloured text (Font8x8_PrintColour)
- *
- * PINK 15.05.26
- *************************************************************************************
-*/
+ * Demonstrates runtime conversion from PI1 font sheets to sFont, then prints with
+ * FontPrint, FontPrintLeft, FontPrintRight and FontPrintCentred.
+ *************************************************************************************/
 
-/* ###################################################################################
-#  INCLUDES
-################################################################################### */
+#include <stdio.h>
 
-#include	<godlib/gemdos/gemdos.h>
-#include	<godlib/font8x8/font8x8.h>
-#include	<godlib/graphic/graphic.h>
-#include	<godlib/ikbd/ikbd.h>
-#include	<godlib/memory/memory.h>
-#include	<godlib/system/system.h>
-#include	<godlib/vbl/vbl.h>
-#include	<godlib/video/video.h>
+#include <godlib/file/file.h>
+#include <godlib/font/font.h>
+#include <godlib/gemdos/gemdos.h>
+#include <godlib/graphic/graphic.h>
+#include <godlib/ikbd/ikbd.h>
+#include <godlib/memory/memory.h>
+#include <godlib/pictypes/degas.h>
+#include <godlib/platform/platform.h>
+#include <godlib/screen/screen.h>
+#include <godlib/sprite/sprite.h>
+#include <godlib/system/system.h>
+#include <godlib/vbl/vbl.h>
+#include <godlib/video/video.h>
 
 
-/* ###################################################################################
-#  PROTOTYPES
-################################################################################### */
+#define SCREEN_WIDTH        320
+#define SCREEN_HEIGHT       200
+#define FONT_SHEET_COLUMNS   20
+#define FONT_CELL_WIDTH      16
+#define FONT_CHAR_FIRST       1
+#define FONT_CHAR_LAST      127
+#define FONT_CHAR_COUNT     (FONT_CHAR_LAST - FONT_CHAR_FIRST + 1)
 
-void	Hardware_Init( void );
-void	Hardware_DeInit( void );
 
-void	Test_Loop( void );
-void	Font_DrawDemo( U16 * apScreen );
-
-/* ###################################################################################
-#	DATA
-################################################################################### */
-
-/* index 0 = black background, 1..15 = distinct colours (STe 0x0RGB) */
-U16				gMyPalette[ 16 ] =
+typedef struct
 {
-	0x000,0xFFF,0xF00,0x0F0,0x00F,0xFF0,0x0FF,0xF0F,
-	0x888,0xF80,0x8F0,0x08F,0xF08,0x80F,0x0F8,0x444
+    const char * mpFileName;
+    U16          mCellHeight;
+    U16          mFixedWidth;
+    U8           mSpaceWidth;
+    U8           mKerning;
+} sFontDef;
+
+
+static sFont * FontExample_LoadPi1Font( const sFontDef * apDef, U16 * apPalette );
+static void    FontExample_DrawPage( sFont * apFont6x6, sFont * apFont8x8, sFont * apFont8x16 );
+static void    FontExample_DrawStaticPage( sFont * apFont6x6, sFont * apFont8x8, sFont * apFont8x16 );
+static void    FontExample_DrawText( S16 aX, S16 aY, sFont * apFont, const char * apText );
+static void    FontExample_DrawTypewriter( sFont * apFont6x6, sFont * apFont8x8, sFont * apFont8x16, const char * apText, S16 aX, S16 aY );
+static void    FontExample_WaitSpace( void );
+
+
+static const sFontDef gFontDefs[] =
+{
+    { "gfx\\FONT6X6.PI1",  6, 6, 6, 0 },
+    { "gfx\\FONT8X8.PI1",  8, 8, 8, 0 },
+    { "gfx\\FONT8X16.PI1", 16, 8, 8, 0 }
 };
 
-/* ###################################################################################
-#  CODE
-################################################################################### */
 
-/*-----------------------------------------------------------------------------------*
-* FUNCTION : GodLib_Game_Main( S16 aArgCount, char * apArgs[] ) )
-* ACTION   : it begins  ...
-* CREATION : 15.05.26 PNK
-*-----------------------------------------------------------------------------------*/
-
-S16	GodLib_Game_Main( S16 aArgCount, char * apArgs[] )
+S16 GodLib_Game_Main( S16 aArgCount, char * apArgs[] )
 {
-	(void)aArgCount;
-	(void)apArgs;
+    sFont * lpFont6x6;
+    sFont * lpFont8x8;
+    sFont * lpFont8x16;
+    U16     lPalette[ 16 ];
 
-	GemDos_Super( 0 );
+    (void)aArgCount;
+    (void)apArgs;
 
-	Hardware_Init();
+    GemDos_Super( 0 );
+    Platform_Init();
 
-	Test_Loop();
+    lpFont6x6  = FontExample_LoadPi1Font( &gFontDefs[ 0 ], &lPalette[ 0 ] );
+    lpFont8x8  = FontExample_LoadPi1Font( &gFontDefs[ 1 ], 0 );
+    lpFont8x16 = FontExample_LoadPi1Font( &gFontDefs[ 2 ], 0 );
 
-	Hardware_DeInit();
+    if( !lpFont6x6 || !lpFont8x8 || !lpFont8x16 )
+    {
+        printf( "Font PI1 files not found in gfx\\\r\n" );
+        GemDos_Cnecin();
+    }
+    else
+    {
+        Screen_Init( SCREEN_WIDTH, SCREEN_HEIGHT, eGRAPHIC_COLOURMODE_4PLANE, eSCREEN_SCROLL_NONE );
+        Video_SetPalST( &lPalette[ 0 ] );
+        Graphic_SetBlitterEnable( BLT_BLITTER );
 
-	return( 0 );
+        FontExample_DrawPage( lpFont6x6, lpFont8x8, lpFont8x16 );
+        FontExample_WaitSpace();
+
+        Screen_DeInit();
+    }
+
+    Font_Destroy( lpFont8x16 );
+    Font_Destroy( lpFont8x8 );
+    Font_Destroy( lpFont6x6 );
+
+    Platform_DeInit();
+
+    return( 0 );
 }
 
 
-/*-----------------------------------------------------------------------------------*
-* FUNCTION : Hardware_Init( void )
-* ACTION   : Hardware_Init
-* CREATION : 15.05.26 PNK
-*-----------------------------------------------------------------------------------*/
-
-void	Hardware_Init( void )
+static sFont * FontExample_LoadPi1Font( const sFontDef * apDef, U16 * apPalette )
 {
-	System_Init();
-	Vbl_Init();
-	Video_Init();
-	IKBD_Init();
+    sDegas *       lpPicture;
+    sSpriteBlock * lpBlock;
+    sFont *        lpFont;
+    U8             lCharMap[ FONT_CHAR_COUNT + 1 ];
+    U16 *          lpMask;
+    U16            i;
+    U16            lChar;
+    U16            lX;
+    U16            lY;
+    U32            lOffset;
+
+    lpPicture = File_Load( apDef->mpFileName );
+    if( !lpPicture )
+    {
+        return( 0 );
+    }
+
+    if( apPalette )
+    {
+        Memory_Copy( sizeof(U16) * 16, &lpPicture->mHeader.mPalette[ 0 ], apPalette );
+    }
+
+    lpBlock = Sprite_BlockCreate( FONT_CHAR_COUNT );
+    if( !lpBlock )
+    {
+        File_UnLoad( lpPicture );
+        return( 0 );
+    }
+
+    for( i=0; i<FONT_CHAR_COUNT; i++ )
+    {
+        lChar = (U16)(FONT_CHAR_FIRST + i);
+        lX    = (U16)((lChar % FONT_SHEET_COLUMNS) * FONT_CELL_WIDTH);
+        lY    = (U16)((lChar / FONT_SHEET_COLUMNS) * apDef->mCellHeight);
+
+        lOffset = ((U32)(lX >> 4) << 2);
+        lOffset += (U32)lY * 80UL;
+
+        lpMask = Sprite_MaskCreate( &lpPicture->mPixels[ lOffset ], FONT_CELL_WIDTH, apDef->mCellHeight, 4, 4, 0 );
+        lpBlock->mpSprite[ i ] = Sprite_Create( &lpPicture->mPixels[ lOffset ], lpMask, FONT_CELL_WIDTH, apDef->mCellHeight, 4, 4, 0 );
+        Sprite_MaskDestroy( lpMask );
+
+        lCharMap[ i ] = (U8)lChar;
+    }
+    lCharMap[ FONT_CHAR_COUNT ] = 0;
+
+    lpFont = Font_Create( lpBlock, &lCharMap[ 0 ], apDef->mFixedWidth );
+    if( lpFont )
+    {
+        lpFont->mSpaceWidth = apDef->mSpaceWidth;
+        lpFont->mKerning    = apDef->mKerning;
+    }
+
+    Sprite_BlockDestroy( lpBlock );
+    File_UnLoad( lpPicture );
+
+    return( lpFont );
 }
 
 
-/*-----------------------------------------------------------------------------------*
-* FUNCTION : Hardware_DeInit( void )
-* ACTION   : Hardware_DeInit
-* CREATION : 15.05.26 PNK
-*-----------------------------------------------------------------------------------*/
-
-void	Hardware_DeInit( void )
+static void FontExample_DrawPage( sFont * apFont6x6, sFont * apFont8x8, sFont * apFont8x16 )
 {
-	IKBD_DeInit();
-	Video_DeInit();
-	Vbl_DeInit();
-	System_DeInit();
+    FontExample_DrawStaticPage( apFont6x6, apFont8x8, apFont8x16 );
+    Screen_Update();
+    Vbl_WaitVbls( 35 );
+
+    FontExample_DrawTypewriter( apFont6x6, apFont8x8, apFont8x16, "TYPEWRITER EFFECT: ONE LETTER PER FRAME.", 0, 166 );
 }
 
 
-/*-----------------------------------------------------------------------------------*
-* FUNCTION : Test_Loop( void )
-* ACTION   : Test_Loop
-* CREATION : 15.05.26 PNK
-*-----------------------------------------------------------------------------------*/
-
-void	Test_Loop( void )
+static void FontExample_DrawStaticPage( sFont * apFont6x6, sFont * apFont8x8, sFont * apFont8x16 )
 {
-	U32		lBase;
-	void *	lpScreenMem;
-	U16 *	lpPhysic;
+    sGraphicRect lRect;
 
-	/* set to a 320x200 16 colour screen*/
-	Video_SetResolution( 320, 200, eGRAPHIC_COLOURMODE_4PLANE, 320 );
+    Screen_Logic_ClearScreen();
 
-	/* allocate screen compatible memory*/
-	lpScreenMem = mMEMSCREENCALLOC( 32000L + 255L );
+    FontExample_DrawText( 8, 8, apFont8x16, "GODLIB FONT EXAMPLE" );
+    FontExample_DrawText( 8, 28, apFont6x6, "PI1 sheets loaded and converted at runtime" );
 
-	if( lpScreenMem )
-	{
-		/* all screen buffers have to be aligned to nearest 256 bytes*/
-		lBase       = (U32)lpScreenMem;
-		lBase       += 255L;
-		lBase      &= 0xFFFFFF00L;
+    FontExample_DrawText( 8, 46, apFont8x8, "Screen_Logic_FontPrint" );
 
-		lpPhysic    = (U16*)lBase;
+    lRect.mX      = 8;
+    lRect.mY      = 66;
+    lRect.mWidth  = 304;
+    lRect.mHeight = 16;
+    Graphic_FontPrintLeft( Screen_GetpLogicGraphic(), apFont8x8, &lRect, "Graphic_FontPrintLeft" );
 
-		Memory_Clear( 32000L, lpPhysic );
+    lRect.mY = 84;
+    Graphic_FontPrintRight( Screen_GetpLogicGraphic(), apFont8x8, &lRect, "Graphic_FontPrintRight" );
 
-		Video_SetPalST( &gMyPalette[ 0 ] );
-		Font_DrawDemo( lpPhysic );
-		Video_SetPhysic( lpPhysic );
+    lRect.mY = 104;
+    Graphic_FontPrintCentred( Screen_GetpLogicGraphic(), apFont8x16, &lRect, "FontPrintCentered" );
 
-		/* wait for space to be pressed */
-		while( !IKBD_GetKeyStatus(eIKBDSCAN_SPACE) )
-		{
-			IKBD_Update();
-		}
-
-		mMEMSCREENFREE( lpScreenMem );
-	}
+    FontExample_DrawText( 8, 132, apFont6x6, "6x6 size" );
+    FontExample_DrawText( 92, 130, apFont8x8, "8x8 size" );
+    FontExample_DrawText( 184, 124, apFont8x16, "8x16 size" );
 }
 
 
-/*-----------------------------------------------------------------------------------*
-* FUNCTION : Font_DrawDemo( U16 * apScreen )
-* ACTION   : draws a title in the default font plus one line per palette colour
-* CREATION : 15.05.26 PNK
-*-----------------------------------------------------------------------------------*/
-
-void	Font_DrawDemo( U16 * apScreen )
+static void FontExample_DrawText( S16 aX, S16 aY, sFont * apFont, const char * apText )
 {
-	char	lLine[ 16 ];
-	U16		lColour;
-	U16		lY;
+    sGraphicPos lPos;
 
-	/* original single-colour printer (writes plane 0 only) */
-	Font8x8_Print( "FONT8X8 DEMO - PRESS SPACE", apScreen, 8, 4 );
-
-	/* coloured printer - one label per palette index 1..15 */
-	for( lColour=1; lColour<16; lColour++ )
-	{
-		lLine[ 0 ] = 'C';
-		lLine[ 1 ] = 'O';
-		lLine[ 2 ] = 'L';
-		lLine[ 3 ] = 'O';
-		lLine[ 4 ] = 'U';
-		lLine[ 5 ] = 'R';
-		lLine[ 6 ] = ' ';
-		lLine[ 7 ] = (char)('0' + (lColour / 10));
-		lLine[ 8 ] = (char)('0' + (lColour % 10));
-		lLine[ 9 ] = 0;
-
-		lY = (U16)(20 + ((lColour - 1) * 11));
-		Font8x8_PrintColour( lLine, apScreen, 8, lY, lColour );
-	}
+    lPos.mX = aX;
+    lPos.mY = aY;
+    Screen_Logic_FontPrint( &lPos, apFont, apText );
 }
 
 
-/* ################################################################################ */
+static void FontExample_DrawTypewriter( sFont * apFont6x6, sFont * apFont8x8, sFont * apFont8x16, const char * apText, S16 aX, S16 aY )
+{
+    char        lBuffer[ 64 ];
+    sGraphicPos lPos;
+    U16         i;
+
+    lPos.mX = aX;
+    lPos.mY = aY;
+
+    i = 0;
+    while( apText[ i ] && (i < (sizeof(lBuffer) - 1)) )
+    {
+        lBuffer[ i ] = apText[ i ];
+        i++;
+        lBuffer[ i ] = 0;
+
+        FontExample_DrawStaticPage( apFont6x6, apFont8x8, apFont8x16 );
+        Screen_Logic_FontPrint( &lPos, apFont8x8, &lBuffer[ 0 ] );
+        Screen_Update();
+        Vbl_WaitVbls( 3 );
+    }
+}
+
+
+static void FontExample_WaitSpace( void )
+{
+    while( !IKBD_GetKeyStatus( eIKBDSCAN_SPACE ) )
+    {
+        IKBD_Update();
+        Vbl_WaitVbl();
+    }
+}
